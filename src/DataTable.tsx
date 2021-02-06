0866e2687764d3340a5a88f90671209c0e8d8cdb
togenyan <nyantoge@gmail.com>
2021-02-06 20:42:14 +0900

    react-datagrid

--- src/DataTable.tsx
+++ src/DataTable.tsx
@@ -1,133 +1,56 @@
 import {
   makeStyles,
+  createStyles,
 } from '@material-ui/core'
 import Box from '@material-ui/core/Box'
-import List from '@material-ui/core/List'
-import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import CardHeader from '@material-ui/core/CardHeader'
 import TextIcon from '@material-ui/icons/FontDownload'
-import FloatIcon from '@material-ui/icons/Gradient'
-import UnknownIcon from '@material-ui/icons/Help'
-import IntegerIcon from '@material-ui/icons/LooksOne'
+import Typography from '@material-ui/core/Typography'
 import React, { useState } from 'react'
 
 import { forwardRef } from 'react';
 
 import AddBox from '@material-ui/icons/AddBox';
-import ArrowDownward from '@material-ui/icons/ArrowDownward';
-import Check from '@material-ui/icons/Check';
-import ChevronLeft from '@material-ui/icons/ChevronLeft';
-import ChevronRight from '@material-ui/icons/ChevronRight';
-import Clear from '@material-ui/icons/Clear';
-import DeleteOutline from '@material-ui/icons/DeleteOutline';
-import Edit from '@material-ui/icons/Edit';
-import FilterList from '@material-ui/icons/FilterList';
-import FirstPage from '@material-ui/icons/FirstPage';
-import LastPage from '@material-ui/icons/LastPage';
-import Remove from '@material-ui/icons/Remove';
-import SaveAlt from '@material-ui/icons/SaveAlt';
-import Search from '@material-ui/icons/Search';
-import ViewColumn from '@material-ui/icons/ViewColumn';
-import MaterialTable from 'material-table'
+import DataGrid from 'react-data-grid'
 
 import type { CfgBinFile, Code, CodeGroup, KeyTable, StringTable, Value, ValueTag } from './types'
 
-const tableIcons = {
-  Add: forwardRef((props, ref) => <AddBox {...props} ref={ref} />),
-  Check: forwardRef((props, ref) => <Check {...props} ref={ref} />),
-  Clear: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Delete: forwardRef((props, ref) => <DeleteOutline {...props} ref={ref} />),
-  DetailPanel: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  Edit: forwardRef((props, ref) => <Edit {...props} ref={ref} />),
-  Export: forwardRef((props, ref) => <SaveAlt {...props} ref={ref} />),
-  Filter: forwardRef((props, ref) => <FilterList {...props} ref={ref} />),
-  FirstPage: forwardRef((props, ref) => <FirstPage {...props} ref={ref} />),
-  LastPage: forwardRef((props, ref) => <LastPage {...props} ref={ref} />),
-  NextPage: forwardRef((props, ref) => <ChevronRight {...props} ref={ref} />),
-  PreviousPage: forwardRef((props, ref) => <ChevronLeft {...props} ref={ref} />),
-  ResetSearch: forwardRef((props, ref) => <Clear {...props} ref={ref} />),
-  Search: forwardRef((props, ref) => <Search {...props} ref={ref} />),
-  SortArrow: forwardRef((props, ref) => <ArrowDownward {...props} ref={ref} />),
-  ThirdStateCheck: forwardRef((props, ref) => <Remove {...props} ref={ref} />),
-  ViewColumn: forwardRef((props, ref) => <ViewColumn {...props} ref={ref} />)
-};
-
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => createStyles({
   card: {
-    margin: '20px'
+    margin: theme.spacing(2),
   },
-})
-
-interface DataValueListItemProps {
-  value: Value
-  stringTable: StringTable
-}
-const DataValueListItem: React.FC<DataValueListItemProps> = ({
-  value,
-  stringTable,
-}) => (
-  <ListItem>
-    <ListItemIcon>
-      {value.tag === "string" ? (
-        <TextIcon />
-      ) : value.tag === "int" ? (
-        <IntegerIcon />
-      ) : value.tag === "float" ? (
-        <FloatIcon />
-      ) : (
-              <UnknownIcon />
-            )}
-    </ListItemIcon>
-    <ListItemText>
-      {value.tag === "string" ? stringTable[value.value] : value.value}
-    </ListItemText>
-  </ListItem>
-)
+}))
 
 interface DataTableProps {
-  title: string
   columns: ValueTag[]
   codes: Code[]
   stringTable: StringTable
 }
 const DataTable: React.FC<DataTableProps> = ({
-  title,
   columns,
   codes,
   stringTable,
 }) => {
-  const [tableColumns, setTableColumns] = useState(columns.map((col, idx) => ({
-    title: col,
-    field: idx.toString(10),
-  })))
-  const [data, setData] = useState(
-    codes.map((code) => code.values.map(
-      (val) => {
-        if (val.tag === "string") {
-          if (val.value === 0xFFFFFFFF) {
-            return "[none]"
-          }
-          return stringTable[val.value]
-        }
-        return val.value
-      }
-    ))
-  )
+  const tableColumns = columns.map((col, idx) => ({
+    name: col,
+    key: idx.toString(10),
+  }))
+  const data = codes.map((code) =>
+    code.values.reduce((acc, curr, idx) => {
+      acc[idx.toString(10)] = (curr.tag === "string") ?
+        (curr.value === 0xFFFFFFFF) ? "[none]"
+          : stringTable[curr.value]
+        : curr.value.toString(10);
+      return acc
+    },
+      {} as Record<string, string>))
   return (
-    <MaterialTable
-      title={title}
-      icons={tableIcons}
+    <DataGrid
       columns={tableColumns}
-      data={data}
-      editable={{
-        onBulkUpdate: changes =>
-          new Promise<void>((resolve) => {
-            setTimeout(() => {
-              resolve();
-            }, 500);
-          })
-      }}
+      rows={data}
+      edit
     />
   )
 }
@@ -140,14 +63,18 @@ interface DataCardProps {
 export const DataCard: React.FC<DataCardProps> = ({ keyTable, stringTable, codeGroup }) => {
   const classes = useStyles()
   return (
-    <Box className={classes.card}>
-      <DataTable
-        title={keyTable[codeGroup.crc32]}
-        columns={codeGroup.columns}
-        codes={codeGroup.codes}
-        stringTable={stringTable}
-      />
-    </Box>
+    <Card className={classes.card}>
+      <CardContent>
+        <Typography variant="h6">
+          {keyTable[codeGroup.crc32]}
+        </Typography>
+        <DataTable
+          columns={codeGroup.columns}
+          codes={codeGroup.codes}
+          stringTable={stringTable}
+        />
+      </CardContent>
+    </Card >
   )
 }
 
